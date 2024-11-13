2db9892d8d1229c739a29b23e41e838329ee2017
zhixiaosu@gmail.com <sue.su@alleninstitute.org>
2024-11-13 19:50:11 +0000

    Udpate

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -1,7 +1,52 @@
 """ top level run script """
 
-def run():
-    """ basic run function """
-    pass
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt 
+import cv2 
+import os
+from pathlib import Path
+import json
+from video_utils import video_info_check, cal_video_temporal_qm
 
-if __name__ == "__main__": run()
+if __name__ == "__main__": 
+    # this needs to be from the user input
+    dir = '/root/capsule/data/behavior_711042_2024-09-13_09-19-15/behavior-videos/bottom_camera.avi'
+    video_info = video_info_check(dir)
+    os.makedirs(video_info['output_dir'], exist_ok=True)
+    json_file = os.path.join(video_info['output_dir'], 'video_qm.json')
+    print(json_file)
+    # will update to create if none, append if exist
+    with open(os.path.join(video_info['output_dir'], "video_qm.json"), "w") as file:
+        json.dump(video_info, file, indent=4, sort_keys=True)
+    file.close()
+    
+    hist_iti, hist_ifi, hist_joint, hist_joint_peak, video_temporal_qm = cal_video_temporal_qm(video_info)
+
+    # Read the existing data
+    with open(json_file, 'r') as file:
+        try:
+            existing_data = json.load(file)
+        except json.JSONDecodeError:
+            existing_data = []  # Initialize as an empty list if file is empty
+
+    # Check if the file is a list or dictionary and append accordingly
+    if isinstance(existing_data, list):
+        existing_data.append(video_temporal_qm)
+    elif isinstance(existing_data, dict):
+        existing_data.update(video_temporal_qm)
+    else:
+        print("Unexpected JSON structure")
+
+    # Write the updated data back to the file
+    with open(json_file, 'w') as file:
+        json.dump(existing_data, file, indent=4, sort_keys=True)
+
+    # save figures
+    hist_ifi.savefig(os.path.join(video_info['output_dir']), 'hist_interframe-intervals.png')
+    hist_iti.savefig(os.path.join(video_info['output_dir']), 'hist_intertime-intervals.png')
+    hist_joint.savefig(os.path.join(video_info['output_dir']), 'joint_dist_interframe-intervals_intertime-intervals.png')
+    hist_joint_peak.savefig(os.path.join(video_info['output_dir']), 'joint_dist_interframe-intervals_intertime-intervals95quantile.png')
+
+
--- code/video_utils.py
+++ code/video_utils.py
@@ -1,6 +1,59 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
-import matlibplit.pyplot as plt 
-import opencv_python as open_cv 
+import matplotlib.pyplot as plt 
+import cv2 
+import os
+from pathlib import Path
+import json
 
+def video_info_check(dir):
+    base_name = os.path.splitext(os.path.basename(dir))[0]
+    path_name = os.path.dirname(dir)
+    video_info = dict()
+    video_info['video_file'] = dir
+    video_info['video_exist'] = os.path.exists(video_info['video_file'])
+    video_info['timestamps_file'] = os.path.join(path_name, f"{base_name}.csv")
+    video_info['timestamps_exist'] = os.path.exists(video_info['timestamps_file'])
+    video_info['output_dir'] = os.path.join('/root/capsule/results', base_name)
+    return video_info  
+
+def cal_video_temporal_qm(video_info):
+    video_temporal_qm = dict()
+    video_temporal_qm['IFI_cdf_quantile'] = list(np.arange(0, 105, 5))
+    video_temporal_qm['IFI_cdf_value'] = None
+    video_temporal_qm['IFI_range'] = None
+    video_temporal_qm['ITI_cdf_quantile'] = list(np.arange(0, 105, 5))
+    video_temporal_qm['ITI_cdf_value'] = None
+    video_temporal_qm['ITI_range'] = None
+    video_temporal_qm['frame_count'] = None
+    video_temporal_qm['timestampe_count'] = None
+    hist_iti = None
+    hist_ifi = None
+    if not video_info['video_exist']:
+        raise ValueError('Target video does not exist')
+    else:
+        video = cv2.VideoCapture(video_info['video_file'])
+        video_temporal_qm['frame_count'] = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+        if video_info['timestamps_exist']:
+            video_meta_data = pd.read_csv(video_info['timestamps_file'], header = None)
+            video_temporal_qm['timestampe_count'] = len(video_meta_data)
+            ITI = np.diff(video_meta_data[0]*1000)
+            IFI = np.diff(video_meta_data[2]/1000000)
+            video_temporal_qm['IFI_cdf_value'] = list(np.percentile(IFI, video_temporal_qm['IFI_cdf_quantile']))
+            video_temporal_qm['ITI_cdf_value'] = list(np.percentile(ITI, video_temporal_qm['ITI_cdf_quantile']))
+            video_temporal_qm['IFI_range'] = [np.min(IFI), np.max(IFI)]
+            video_temporal_qm['ITI_range'] = [np.min(ITI), np.max(ITI)]
+            hist_joint_all = sns.jointplot(x = ITI, y = IFI, kind = 'hist', bins = 10,
+                       stat='probability',marginal_ticks=True, 
+                       marginal_kws=dict(bins=20, fill=True, stat='probability')).set_axis_labels(xlabel = 'Harp time (ms)', ylabel='Camera time')
+            mode_range  = (ITI>=video_temporal_qm['ITI_cdf_value'][1]) & (ITI<=video_temporal_qm['ITI_cdf_value'][-2]) & (IFI>=video_temporal_qm['IFI_cdf_value'][1]) & (IFI <=video_temporal_qm['IFI_cdf_value'][-2])
+            mode_range  = (ITI<=video_temporal_qm['ITI_cdf_value'][-2]) & (IFI<=video_temporal_qm['IFI_cdf_value'][-2])
+            hist_joint_peak = sns.jointplot(x = ITI[mode_range], y = IFI[mode_range], kind = 'hist', bins = 10,
+                       stat='probability',marginal_ticks=True, 
+                       marginal_kws=dict(bins=20, fill=True, stat='probability')).set_axis_labels(xlabel = 'Harp time (ms)', ylabel='Camera time (ms)')
+            hist_iti, ax = plt.subplots()
+            ax.hist(ITI, bins=30)
+            hist_ifi, ax = plt.subplots()
+            ax.hist(IFI, bins=30)
+    return hist_iti, hist_ifi, hist_joint_all, hist_joint_peak, video_temporal_qm
