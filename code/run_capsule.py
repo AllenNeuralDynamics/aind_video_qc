1b92bbebab681c053222adda59988472480a4446
1mattbecker <matt.becker@alleninstitute.org>
2024-11-18 22:38:05 +0000

    incorporated functions for pixel analysis metrics

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -8,7 +8,7 @@ import cv2
 import os
 from pathlib import Path
 import json
-from video_utils import video_info_check, cal_video_temporal_qm
+from video_utils import video_info_check, cal_video_temporal_qm, extract_frames
 
 if __name__ == "__main__": 
     # this needs to be from the user input
@@ -49,4 +49,30 @@ if __name__ == "__main__":
     hist_joint.savefig(os.path.join(video_info['output_dir'], 'joint_dist_interframe-intervals_intertime-intervals.png'))
     hist_joint_peak.savefig(os.path.join(video_info['output_dir'], 'joint_dist_interframe-intervals_intertime-intervals95quantile.png'))
 
+    # extract frames for image quality metrics
+    frames = extract_frames(video_info, n_frames=1000)
 
+    # generate reports on focus, contrast, intensity
+    video_pixel_metrics = dict()
+    video_pixel_metrics['focus_quantiles'], video_pixel_metrics['focus_quantile_values'] = generate_focus_report(frames, focus_threshold=60, output_dir=os.path.join(video_info['output_dir'], 'focus_analysis_output'))
+    video_pixel_metrics['contrast_quantiles'], video_pixel_metrics['contrast_quantile_values'] = generate_contrast_report(frames, contrast_threshold=1, output_dir=os.path.join(video_info['output_dir'], 'contrast_analysis_output'))
+    video_pixel_metrics['intensity_quantiles'], video_pixel_metrics['intensity_quantile_values'] = generate_intensity_report(frames, lower_threshold=30, upper_threshold=40, output_dir=os.path.join(video_info['output_dir'], 'intensity_analysis_output'))
+
+    # Read the existing data
+    with open(json_file, 'r') as file:
+        try:
+            existing_data = json.load(file)
+        except json.JSONDecodeError:
+            existing_data = []  # Initialize as an empty list if file is empty
+
+    # Check if the file is a list or dictionary and append accordingly
+    if isinstance(existing_data, list):
+        existing_data.append(video_pixel_metrics)
+    elif isinstance(existing_data, dict):
+        existing_data.update(video_pixel_metrics)
+    else:
+        print("Unexpected JSON structure")
+
+    # Write the updated data back to the file
+    with open(json_file, 'w') as file:
+        json.dump(existing_data, file, indent=4, sort_keys=True)
